53d9ebfde230e11ff91800e9aa97e7922836293b
kplnhsyn <kplnhsyn.49@gmail.com>
2023-11-26 14:29:35 +0100

    debugging and logging

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(require("cors")());
 app.use(require("./src/middlewares/findSearchSortPage"));
 app.use(require("./src/middlewares/authentication"));
+app.use(require('./src/middlewares/logger'))
 /* ------------------------------------------------------------------------- */
 // ROUTES:
 app.use(require("./src/routes"));
